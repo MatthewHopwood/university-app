ab334c0cd5e91559de97adf8c04f722bae96a520
matthewhopwood <matthewhopwood21@hotmail.com>
2019-07-26 09:27:57 +0100

    add route for course_enrol and fix typos

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
   resources :students, except: [:destroy]
   get 'login', to: 'logins#new'
-  post 'loging', to: 'logins#create'
+  post 'login', to: 'logins#create'
   delete 'logout', to: 'logins#destroy'
+  post 'course_enrol', to: 'student_courses#create'
 end
